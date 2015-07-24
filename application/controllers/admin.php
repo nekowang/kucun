b29ce81e8718dca6d1faec2891850e4d3ecaacae
wangyulin <773703887@qq.com>
2015-07-24 17:01:27 +0800

    1

---
+++ application/controllers/admin.php
@@ -0,0 +1,18 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Admin extends CI_Controller {
+    
+        public function __construct() {
+            parent::__construct();
+                //$this->load->model('admin_model');
+        }
+
+	public function index()
+	{
+		$this->load->view('admin');
+	}
+        
+}
+
+/* End of file login.php */
+/* Location: ./application/controllers/login.php */
---
+++ application/controllers/login.php
@@ -0,0 +1,29 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Login extends CI_Controller {
+    
+        public function __construct() {
+            parent::__construct();
+                //$this->load->model('admin_model');
+        }
+
+	public function index(){
+            $this->load->view('login');
+	}
+        
+        public function verify(){
+            $username = $this->input->post('username');
+            $password = $this->input->post('password');
+            
+            $url = preg_replace('/\?error=false/', '', $_SERVER['HTTP_REFERER']);
+            
+            if($username == 'admin' && $password =='admin'){
+                header("location:/admin/index");
+            } else {
+                header("location:$url.?error=false");
+            }
+        }
+}
+
+/* End of file login.php */
+/* Location: ./application/controllers/login.php */
