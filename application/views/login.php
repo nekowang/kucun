95e924e0011d1262fa65e651b19939c52c23c14a
wangyulin <773703887@qq.com>
2015-07-30 14:16:44 +0800

    sssss

--- application/views/login.php
+++ application/views/login.php
@@ -1,76 +1,77 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title>库存管理系统</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="description" content="库存管理系统">
-        <meta name="author" content="">
-
-        <link href="/css/bootstrap.css" rel="stylesheet">
-        <style type="text/css">
-            body {
-                padding-top: 40px;
-                padding-bottom: 40px;
-                background-color: #f5f5f5;
-            }
-
-            .form-signin {
-                max-width: 300px;
-                padding: 19px 29px 29px;
-                margin: 0 auto 20px;
-                background-color: #fff;
-                border: 1px solid #e5e5e5;
-                -webkit-border-radius: 5px;
-                -moz-border-radius: 5px;
-                border-radius: 5px;
-                -webkit-box-shadow: 0 1px 2px rgba(0,0,0,.05);
-                -moz-box-shadow: 0 1px 2px rgba(0,0,0,.05);
-                box-shadow: 0 1px 2px rgba(0,0,0,.05);
-            }
-            .form-signin .form-signin-heading,
-            .form-signin .checkbox {
-                margin-bottom: 10px;
-            }
-            .form-signin input[type="text"],
-            .form-signin input[type="password"] {
-                font-size: 16px;
-                height: auto;
-                margin-bottom: 15px;
-                padding: 7px 9px;
-            }
-
-        </style>
-        <link href="/css/bootstrap-responsive.css" rel="stylesheet">
-
-        <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
-        <!--[if lt IE 9]>
-        <script src="/js/html5shiv.js"></script>
-        <![endif]-->
-
-    </head>
-
-    <body>
-
-        <div class="container">
-
-            <form class="form-signin" action="/login/verify" method="post">
-                <h2 class="form-signin-heading">库存管理系统 v1.0</h2>
-                <input type="text" class="input-block-level" placeholder="用户名" name="username">
-                <input type="password" class="input-block-level" placeholder="密码" name="password">
-                <label class="checkbox">
-                    <input type="checkbox" value="remember-me">记住我 
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+</head>
+<body>
+	<div id="blue"></div>
+	<div id="login">
+	<div id="version"></div>
+	<form action="/login/verify" method="post">
+		<div id="username">
+			用户名&nbsp;&nbsp;&nbsp;<input type="text" name="username" />
+		</div>
+		<div id="pwd">
+			密&nbsp;&nbsp;&nbsp;码&nbsp;&nbsp;&nbsp;<input type="password" name="password" />
+		</div>
+                <div id="logininfo">                    
                     <?php
                     $error = $this->input->get('error');
                     if (!empty($error)) {
-                        echo "<font size='1px' color='red' style='padding-left:130px;'> 用户名或密码错误</font>";
+                        echo "<font size='1px' color='red' style='padding-left:145px;'> 用户名或密码错误</font>";
                     }
-                    ?>
-                </label>
-                <button class="btn btn-large btn-primary" type="submit">登陆</button>
-            </form>
+                    ?></div>
+		<input type="submit" value="登&nbsp&nbsp;录" id="submit" />
+	</form>
+	</div>	
+</body>
+<style>
+input{outline:none;
 
-        </div>
-
-    </body>
-</html>
+}
+input:-webkit-autofill {-webkit-box-shadow: 0 0 0px 1000px white inset;}
+body{background:#808080 no-repeat;}
+#login{
+position:relative;left:35%;top:33%;
+height:350px;width:500px;
+background:#dddddd;
+border-radius:4px;
+box-shadow:0	0 10px #ffffff;
+}
+#version{
+position:absolute;top:-100px;left:-50;height:100px;width:1000px;background:url('/img/version.png') no-repeat;
+}
+#blue{
+position:absolute;left:0;top:38%;width:100%;height:300px;background:#0088cc;
+}
+#submit{
+position:absolute;left:100px;bottom:40px;
+display:block;width:300px;height:40px;background:orange;border:0; 
+font: 20px/38px Microsoft YaHei;color:#efefef;
+border-radius:3px;
+box-shadow:0	0 1px 1px #888888;
+}
+#submit:active {background:#c37e00;color:#cdcdcd;}
+#username{
+float:left;
+height:50px;width:380px;
+margin:70px 70px 20px;
+color:#888888;
+font:bold 20px/48px Microsoft YaHei;
+}
+#username input {background:#efefef;height:40px;width:280px;border:1px #0088cc solid;border-radius:2px;
+font:bold 20px/40px Microsoft YaHei;
+color:#555555;
+padding-left:5px;
+}
+#pwd{
+float:left;
+margin:20px 70px 20px;
+height:50px;width:380px;
+color:#888888;
+font:bold 20px/48px Microsoft YaHei;
+}
+#pwd input {background:#efefef;height:40px;width:281px;border:1px #0088cc solid;border-radius:2px;
+font:bold 20px/40px Microsoft YaHei;
+color:#555555;
+padding-left:5px;
+}
+</style>
